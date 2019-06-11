f5d3652b3b667947bf23f3a5486d8cbce3106010
Dinh <xdinh@dxc.com>
2019-06-11 17:35:49 +0700

    update npm run buid in build.sh 4th

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 ROOT_FOLDER=$PWD
+SOURCE_FOLDER=$PWD/todoApp
+OUTPUT_BUILD_FOLDER=$SOURCE_FOLDER/dist
+DIST_FOLDER=$PWD/dist
 
+mkdir -p $DIST_FOLDER
 cd todoApp
 
 npm install
@@ -9,3 +13,4 @@ npm install @angular/cli
 npm run build
 
 ls -la
+cp -R $OUTPUT_BUILD_FOLDER/* $DIST_FOLDER/
